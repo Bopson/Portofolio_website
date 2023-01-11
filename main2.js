6c41113dbe3abe59341fc52ff90487b2d0a0f728
Baudry <medjiengoue@gmail.com>
2023-01-11 14:09:29 +0200

    responsive changes

--- main2.js
+++ main2.js
@@ -6,4 +6,19 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
+    
   }
+
+  function openNav2() {
+    document.getElementById("left-section").style.display = "block";
+  /*   document.getElementById("left-section").style.opacity = "1";
+    document.getElementById("left-section").style.zIndex = "5";
+
+    document.getElementById("left-section").style.width = "250px"; */
+/*     document.getElementById("main").style.marginRight = "250px";
+ */  }
+  
+  function closeNav2() {
+    document.getElementById("left-section").style.width = "0";
+/*     document.getElementById("main").style.marginRight= "0";
+ */  }
